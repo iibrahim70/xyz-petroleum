14bc13f8a6a9f0c728b4ece860279e0776f57598
iibrahim70 <aviknuhash70@gmail.com>
2023-08-19 17:30:53 +0600

    added result page pdf downloader and basic changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Form from "./Form.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Form from "./pages/Form.jsx";
+import Result from "./pages/Result.jsx";
 import "./index.css";
-import Result from "./Result.jsx";
-import { FormProvider } from "./componants/FormContext.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Form />,
-  },
-  {
-    path: "/results",
-    element: <Result />,
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Form />,
+      },
+      {
+        path: "/results",
+        element: <Result />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FormProvider>
-      <RouterProvider router={router} />
-    </FormProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
+import StepOneFields from "../componants/StepOneFields";
+import StepTwoFields from "../componants/StepTwoFields";
 import { useForm } from "react-hook-form";
-import { useFormContext } from "./componants/FormContext";
-import StepOneFields from "./componants/StepOneFields";
-import StepTwoFields from "./componants/StepTwoFields";
 import { useNavigate } from "react-router-dom";
 
 const Form = () => {
   const [step, setStep] = useState(1);
   const navigate = useNavigate();
-
-  const { formData, updateFormData } = useFormContext();
 
   const {
     handleSubmit,
@@ -19,23 +16,21 @@ const Form = () => {
 
   const handleNext = (data) => {
     setStep(step + 1);
-    updateFormData({ ...data });
   };
 
   const handlePrevious = () => {
     setStep(step - 1);
   };
 
-  const handleFinish = (data) => {
-    updateFormData({ ...formData, ...data });
-    console.log(formData);
+  const onSubmit = (data) => {
+    localStorage.setItem("formData", JSON.stringify(data));
     navigate("/results");
   };
 
   return (
     <form
-      onSubmit={handleSubmit(step === 1 ? handleNext : handleFinish)}
-      className="space-y-4 w-[90%] md:w-[50%] mx-auto flex justify-center flex-col min-h-screen"
+      onSubmit={handleSubmit(step === 1 ? handleNext : onSubmit)}
+      className="space-y-4 w-[90%] md:w-[50%] mx-auto my-10 lg:my-20"
     >
       {step === 1 && (
         <>
@@ -50,7 +45,7 @@ const Form = () => {
         </>
       )}
       {step === 2 && (
-        <div className="my-10 lg:my-20">
+        <>
           <h2 className="text-xl font-semibold">
             Step 2: Additional Information
           </h2>
@@ -58,14 +53,14 @@ const Form = () => {
           <StepTwoFields register={register} errors={errors} />
 
           <div className="flex justify-between">
-            <button onClick={handlePrevious} className="secondary-button">
+            <button onClick={handlePrevious} className="primary-button">
               Previous
             </button>
             <button type="submit" className="primary-button">
               Finish
             </button>
           </div>
-        </div>
+        </>
       )}
     </form>
   );
